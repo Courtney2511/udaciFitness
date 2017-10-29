a8890257eeaa37d50cc00eb573df63afdf16cb1c
Courtney Noonan <courtneynoonan@me.com>
2017-10-29 13:34:04 -0400

    adding submit button and beginning to build of submit functionality

--- components/addEntry.js
+++ components/addEntry.js
@@ -1,9 +1,19 @@
 import React, { Component } from 'react'
-import { View, Text } from 'react-native'
-import { getMetricMetaInfo } from '../utils/helpers'
+import { View, TouchableOpacity, Text } from 'react-native'
+import { getMetricMetaInfo, timeToString } from '../utils/helpers'
 import UdaciSlider from './udaciSlider'
 import UdaciStepper from './udaciStepper'
 import DateHeader from './DateHeader'
+
+function SubmitBtn({ onPress }) {
+  return(
+    <TouchableOpacity
+      onPress={onPress}>
+      <Text>Submit</Text>
+    </TouchableOpacity>
+  )
+}
+
 
 export default class AddEntry extends Component {
   state = {
@@ -44,6 +54,24 @@ export default class AddEntry extends Component {
     }))
   }
 
+  submit = () => {
+    const key = timeToString()
+    const entry = this.state
+
+    // update Redux
+
+    this.setState(() => ({
+      run: 0,
+      bike: 0,
+      swim: 0,
+      sleep: 0,
+      eat: 0,
+    }))
+    // navigate to home
+    // Save to 'DB'
+    // clear local notification
+  }
+
   render() {
     const metaInfo = getMetricMetaInfo()
 
@@ -73,6 +101,7 @@ export default class AddEntry extends Component {
             </View>
           )
         })}
+      <SubmitBtn onPress={ this.submit }/>
       </View>
     )
   }
