f582964cb6195444d203b55033bfd1a1c17334ff
Courtney Noonan <courtneynoonan@me.com>
2017-10-29 15:41:42 -0400

    completing the slider component

--- App.js
+++ App.js
@@ -1,8 +1,11 @@
 import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, Slider } from 'react-native'
 import AddEntry from './components/addEntry'
 
 export default class App extends React.Component {
+  state = {
+    value: 0,
+  }
   render() {
     return (
       <View style={ styles.container }>
