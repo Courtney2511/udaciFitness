855020e6bb163df05a2a76e1fcb63e25e5568d73
Courtney Noonan <courtneynoonan@me.com>
2017-10-29 13:16:13 -0400

    adding styles to app

--- App.js
+++ App.js
@@ -1,27 +1,22 @@
 import React from 'react'
-import { View } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 import AddEntry from './components/addEntry'
 
 export default class App extends React.Component {
-  componentDidMount() {
-    console.log('before')
-    debugger
-    console.log('after')
-  }
   render() {
     return (
-      <View>
+      <View style={ styles.container }>
         <AddEntry />
       </View>
     );
   }
 }
 
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#fff',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-// });
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
