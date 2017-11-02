5b85a28fac0a6155915036ced22bbab427d8a56f
Courtney Noonan <courtneynoonan@me.com>
2017-11-02 15:41:33 -0400

    adding reducer for entries

---
+++ reducers/index.js
@@ -0,0 +1,17 @@
+import { RECIEVE_ENTRIES, ADD_ENTRY } from '../actions'
+
+function entries(state = {}, action) {
+  switch(action.type) {
+    case: RECIEVE_ENTRIES:
+      return {
+        ...state,
+        ...action.entries
+      }
+    case: ADD_ENTRY
+      return {}
+    default:
+      return state
+  }
+}
+
+export default entries
