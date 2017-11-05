29e33ecca6874719917b4c609458891ee61e2eb5
Courtney Noonan <courtneynoonan@me.com>
2017-11-05 11:14:33 -0500

    adding TabNavigator

--- App.js
+++ App.js
@@ -1,10 +1,55 @@
 import React from 'react'
-import { View, Text, StyleSheet, Slider } from 'react-native'
+import { View, Platform } from 'react-native'
 import AddEntry from './components/addEntry'
 import History from './components/history'
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import reducer from './reducers'
+import { TabNavigator } from 'react-navigation'
+import { purple, white } from './utils/colors'
+import { FontAwesome, Ionicons } from '@expo/vector-icons'
+
+const Tabs = TabNavigator(
+  {
+    History: {
+      screen: History,
+      navigationOptions: {
+        tabBarLabel: 'History',
+        tabBarIcon: ({ tintColor }) => (
+          <Ionicons name="ios-bookmarks" size={30} color={tintColor} />
+        ),
+      },
+    },
+    AddEntry: {
+      screen: AddEntry,
+      navigationOptions: {
+        tabBarLabel: 'Add Entry',
+        tabBarIcon: ({ tintColor }) => (
+          <FontAwesome name="plus-square" size={30} color={tintColor} />
+        ),
+      },
+    },
+  },
+  {
+    navigationOptions: {
+      header: null,
+    },
+    tabBarOptions: {
+      activeTintColor: Platform.OS === 'ios' ? purple : white,
+      style: {
+        height: 56,
+        backgroundColor: Platform.OS === 'ios' ? white : purple,
+        shadowColor: 'rgba(0, 0, 0, 0.24)',
+        shadowOffset: {
+          width: 0,
+          height: 3,
+        },
+        shadowRadius: 6,
+        shadowOpacity: 1,
+      },
+    },
+  }
+)
 
 export default class App extends React.Component {
   state = {
@@ -14,12 +59,11 @@ export default class App extends React.Component {
     return (
       <Provider store={createStore(reducer)}>
         <View style={{ flex: 1 }}>
-          <View style={{ height: 20 }}></View>
-          <History />
+          <View style={{ height: 20 }} />
+          <Tabs />
         </View>
       </Provider>
-
-    );
+    )
   }
 }
 
